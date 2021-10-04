1d519153743c5a7f83aa29d5686e879aed99562f
Olaobaju Abraham <olaobajua@gmail.com>
2021-10-04 16:22:23 +0100

    removed cors from rc

--- server.js
+++ server.js
@@ -46,39 +46,27 @@ var corsOptions = {
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-app.post("/register-chat", async (req, res, next) => {
-  // const { vendor, projectName, influencer } = req.body;
-  // // console.log(influencer)
-  // let registration_status = false;
-  // const callback = (err, success) => {
-  //   if (err) {
-  //     res.status(400);
-  //     res.send({ message: "chat already exist" });
-  //     console.error(err);
-  //   } else {
-  //     res.status(200);
-  //     res.send({ message: "chat registered" });
-  //     console.error(success);
-  //   }
-  // };
+app.post("/register-chat", async (req, res) => {
+  const { vendor, projectName, influencer } = req.body;
+  // console.log(influencer)
+  let registration_status = false;
 
-  // registration_status = boolean(await isChatRegisted({ vendor, influencer }));
+  registration_status = boolean(await isChatRegisted({ vendor, influencer }));
 
-  // if (!registration_status) {
-  //   initateChat.emit("saveDetails", { vendor, projectName, influencer }); // saveDetails eventHandler
-  // }
-  // // Encrypt
-  // var ciphertext = CryptoJS.AES.encrypt(
-  //   JSON.stringify(req.body),
-  //   encrytionKey
-  // ).toString();
+  if (!registration_status) {
+    initateChat.emit("saveDetails", { vendor, projectName, influencer }); // saveDetails eventHandler
+  }
+  // Encrypt
+  var ciphertext = CryptoJS.AES.encrypt(
+    JSON.stringify(req.body),
+    encrytionKey
+  ).toString();
 
-  // let response = Object.assign({}, req.body, {
-  //   id: ciphertext,
-  //   chat_status: registration_status,
-  // });
-  // res.json(response);
-  res.json(req.body);
+  let response = Object.assign({}, req.body, {
+    id: ciphertext,
+    chat_status: registration_status,
+  });
+  res.json(response);
 });
 
 app.get("/rcd", cors(corsOptions), async (req, res) => {
