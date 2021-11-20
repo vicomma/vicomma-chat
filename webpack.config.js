3b77dbb76cc382f03f4be5a0a428841e905b4669
Olaobaju Abraham <olaobajua@gmail.com>
2021-11-20 21:09:39 +0100

    updated server.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, "build"),
+    path: path.join(__dirname, "public/build"),
     filename: "index.js",
   },
   module: {
