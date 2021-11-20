f251a0b07f93e94eb87ae34b3abcd78468d4c281
Olaobaju Abraham <olaobajua@gmail.com>
2021-11-20 21:13:01 +0100

    updated server.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "../src/App";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "../src/App";
 
-var element = document.getElementById("vicomma-chat");
-if (typeof element !== "undefined" && element !== null) {
-  ReactDOM.render(<App />, document.getElementById("vicomma-chat"));
-}
+// var element = document.getElementById("vicomma-chat");
+// if (typeof element !== "undefined" && element !== null) {
+//   ReactDOM.render(<App />, document.getElementById("vicomma-chat"));
+// }
+
+const PORT = process.env.PORT || 3000;
