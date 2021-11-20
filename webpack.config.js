c08dcad7b96f124f9102c9728a85d1a0a2a7bf51
Olaobaju Abraham <olaobajua@gmail.com>
2021-11-20 18:53:03 +0100

    fix: Added Socket link

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, "Test"),
+    path: path.join(__dirname, "build"),
     filename: "index.js",
   },
   module: {
